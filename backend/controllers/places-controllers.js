56c3a9659a99f670bc8e5c3443d42967e6cfdd7f
MansourMoukdad88 <m_sy@outlook.com>
2020-05-10 02:24:38 +0300

    Adding Authorization -UpdatePlace

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -155,6 +155,11 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to edit this place.", 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -184,10 +189,6 @@ const deletePlace = async (req, res, next) => {
       "Something went wrong, could not delete place.",
       500
     );
-    return next(error);
-  }
-  if (place.creator.toString() !== req.userData.userId) {
-    const error = new HttpError("You are not allowed to edit this place.", 401);
     return next(error);
   }
 
